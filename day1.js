857377e81f7e443a86bf687f69a8c47cf4a8b433
Jon <jyorg@outlook.com>
2017-12-01 23:28:41 +0300

    clean up day1 code

--- day1.js
+++ day1.js
@@ -1,8 +1,8 @@
 //  Day 1  (javascript)
 
 const captchaSolver = s => (
-  s.split('').reduce((sum, curr, i) => {
-    const next = (i===s.length-1 ? s.charAt(0) : s.charAt(i+1));
-    return(curr === next ? sum + +curr : sum);
-  }, 0)
+  s.split('').reduce((sum, curr, i) => (
+    curr === s.charAt((i+1)%s.length) ? sum + +curr : sum
+  ), 0)
 )
+

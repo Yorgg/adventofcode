2fc71405ca7d2c3bb7a4d44fe035bd8bd9a2dfa9
Jon <jyorg@outlook.com>
2017-12-01 23:45:30 +0300

    add part 2 to day1

--- day1.js
+++ day1.js
@@ -1,8 +1,18 @@
 //  Day 1  (javascript)
+// http://adventofcode.com/2017/day/1
 
+// part I
 const captchaSolver = s => (
   s.split('').reduce((sum, curr, i) => (
     curr === s.charAt((i+1)%s.length) ? sum + +curr : sum
   ), 0)
 )
+
+// part II
+const captchaSolver = s => (
+  s.split('').reduce((sum, curr, i) => (
+    curr === s.charAt((i+s.length/2)%s.length) ? sum + +curr : sum
+  ), 0)
+)
+
 
